8beb8670dce23b2fdfe890d96727ebee157d8882
Rawnly <fedevitale99@gmail.com>
2021-11-17 13:08:22 +0100

    completed

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/rawnly/git-checkout
 
 go 1.17
 
-require github.com/cli/safeexec v1.0.0
+require github.com/AlecAivazis/survey/v2 v2.3.2
+
+require (
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	golang.org/x/sys v0.0.0-20211116061358-0a5406a5449c // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -2,34 +2,138 @@ package main
 
 import (
 	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+	"os"
 	"os/exec"
+	"strings"
 )
 
-
-func gitBranch() error {
-	out, err := exec.Command("git", "branch").Output()
-
-	if err != nil {
-		return err
-	}
+func gitBranch() ([]string, error) {
+	b, err := exec.Command("git", "branch").Output()
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	fmt.Printf("The data is: %s\n", out)
+	output := string(b)
 
-	return nil
+	output = strings.ReplaceAll(output, "*", "")
+	output = strings.ReplaceAll(output, " ", "")
+
+	branchesList := filter(strings.Split(output, "\n"), func(item string, _ int) bool {
+		return len(strings.TrimSpace(item)) > 0
+	})
+
+	return branchesList, nil
 }
 
-func gitCheckout() {
+func getCurrentBranch() string {
+	var currentbranch string
+	b, err := exec.Command("git", "branch").Output()
 
+	if err != nil {
+		return currentbranch
+	}
+
+	output := string(b)
+	output = strings.ReplaceAll(output, " ", "")
+
+	for _, b := range strings.Split(output, "\n") {
+		if strings.Contains(b, "*") {
+			currentbranch = b
+		}
+	}
+
+	return strings.ReplaceAll(currentbranch, "*", "")
+}
+
+func gitCheckout(branch string, createBranch bool) bool {
+	if branch == "" {
+		return false
+	}
+
+	if createBranch {
+		err := exec.Command("git", "checkout", "-b", branch).Run()
+
+		if err != nil {
+			return false
+		}
+	} else {
+		err := exec.Command("git", "checkout", branch).Run()
+
+		if err != nil {
+			return false
+		}
+	}
+
+
+	return true
+}
+
+func filter(arr []string, predicate func (item string, idx int) bool ) []string {
+	var filteredArr []string
+
+	for i, s := range arr {
+		if predicate(s, i) {
+			filteredArr = append(filteredArr, s)
+		}
+	}
+
+	return filteredArr
 }
 
 func main() {
-	err := gitBranch()
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		branchToCheckout := args[0]
+
+		if args[0] == "-b" {
+			branchToCheckout = args[1]
+
+			if gitCheckout(branchToCheckout, true) {
+				fmt.Printf("Switched to branch '%s'", branchToCheckout)
+				return
+			}
+		} else if branchToCheckout != "" {
+			if gitCheckout(branchToCheckout, false) {
+				fmt.Printf("Switched to branch '%s'", branchToCheckout)
+				return
+			}
+		}
+	}
+
+
+	branches, err := gitBranch()
 
 	if err != nil {
 		panic(err)
 	}
+
+
+	qs := []*survey.Question{
+		{
+			Name: "branch",
+			Prompt: &survey.Select{
+				Message: "Select a branch",
+				Options: branches,
+				Default: getCurrentBranch(),
+			},
+		},
+	}
+
+	answ := struct {
+		Branch string `survey:"branch"`
+	}{}
+
+	err = survey.Ask(qs, &answ)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	gitCheckout(answ.Branch, false)
 }
+
+
